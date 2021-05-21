5e48db097640d60f8f97f70094a1fc9c6311a347
Sergey Pushkovskiy <spushkovskiy@gmail.com>
2021-05-21 08:09:06 +0300

    fixed app and db

--- db.js
+++ db.js
@@ -1,16 +1,23 @@
 const Sequelize = require('sequelize');
-                                //database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
-    host: 'localhost',
-    dialect: 'postgres'
-})
-
-sequelize.authenticate().then(
+require('dotenv').config();
+//database username   password
+const sequelize = new Sequelize(
+  process.env.DB,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  }
+);
+exports.sync = () => {
+  sequelize.authenticate().then(
     function success() {
-        console.log("Connected to DB");
+      console.log('Connected to DB');
     },
 
     function fail(err) {
-        console.log(`Error: ${err}`);
+      console.log(`Error: ${err}`);
     }
-)
+  );
+};
